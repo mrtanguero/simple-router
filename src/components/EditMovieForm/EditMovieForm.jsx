37138e4a54bd9ac39c256b71173148ea0c1a4db7
Ivan Jovanovic <mrtanguero@gmail.com>
2021-05-08 11:18:39 +0200

    Fix edit and new movie pages for responsiveness

--- src/components/EditMovieForm/EditMovieForm.jsx
+++ src/components/EditMovieForm/EditMovieForm.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import _ from 'lodash';
 import { useParams, useHistory } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import _ from 'lodash';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function EditMovieForm({ movies, setMovies }) {
   const { movieId } = useParams();
@@ -36,79 +39,91 @@ export default function EditMovieForm({ movies, setMovies }) {
   };
 
   return (
-    <Form className="w-75 d-flex m-auto" onSubmit={onSubmitHandler}>
-      <div className="image-container" style={{ width: '40%' }}>
-        <img
-          src={movieImageUrl}
-          alt={movieTitle}
-          style={{
-            objectFit: 'cover',
-            width: '100%',
-            height: 'auto',
-          }}
-        />
-      </div>
-      <div className="form-fields-containerpe-5 ps-5" style={{ width: '60%' }}>
-        <Form.Group className="mb-4" controlId="movieName">
-          <Form.Label>Naziv filma</Form.Label>
-          <Form.Control
-            type="text"
-            value={movieTitle}
-            onChange={(e) => setMovieTitle(e.target.value)}
-          />
-        </Form.Group>
+    <Form onSubmit={onSubmitHandler}>
+      <Container>
+        <Row>
+          <Col md={7}>
+            <div className="form-fields-container pe-5 ps-5">
+              <Form.Group className="mb-4" controlId="movieName">
+                <Form.Label>Naziv filma</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={movieTitle}
+                  onChange={(e) => setMovieTitle(e.target.value)}
+                />
+              </Form.Group>
 
-        <Form.Group className="mb-4" controlId="movieYear">
-          <Form.Label>Godina izdavanja</Form.Label>
-          <Form.Control
-            type="number"
-            from={1900}
-            to={2021}
-            value={+movieYear}
-            onChange={(e) => setMovieYear(e.target.value)}
-          />
-        </Form.Group>
+              <Form.Group className="mb-4" controlId="movieYear">
+                <Form.Label>Godina izdavanja</Form.Label>
+                <Form.Control
+                  type="number"
+                  from={1900}
+                  to={2021}
+                  value={+movieYear}
+                  onChange={(e) => setMovieYear(e.target.value)}
+                />
+              </Form.Group>
 
-        <Form.Group className="mb-4" controlId="moveGenre">
-          <Form.Label>Žanr</Form.Label>
-          <Form.Control
-            type="text"
-            value={movieGenre}
-            onChange={(e) => setMovieGenre(e.target.value)}
-          />
-        </Form.Group>
+              <Form.Group className="mb-4" controlId="moveGenre">
+                <Form.Label>Žanr</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={movieGenre}
+                  onChange={(e) => setMovieGenre(e.target.value)}
+                />
+              </Form.Group>
 
-        <Form.Group className="mb-4" controlId="moveDirector">
-          <Form.Label>Režija</Form.Label>
-          <Form.Control
-            type="text"
-            value={movieDirector}
-            onChange={(e) => setMovieDirector(e.target.value)}
-          />
-        </Form.Group>
+              <Form.Group className="mb-4" controlId="moveDirector">
+                <Form.Label>Režija</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={movieDirector}
+                  onChange={(e) => setMovieDirector(e.target.value)}
+                />
+              </Form.Group>
 
-        <Form.Group className="mb-4" controlId="moveActors">
-          <Form.Label>Glumci</Form.Label>
-          <Form.Control
-            type="text"
-            value={movieActors}
-            onChange={(e) => setMovieActors(e.target.value)}
-          />
-        </Form.Group>
+              <Form.Group className="mb-4" controlId="moveActors">
+                <Form.Label>Glumci</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={movieActors}
+                  onChange={(e) => setMovieActors(e.target.value)}
+                />
+              </Form.Group>
 
-        <Form.Group className="mb-4" controlId="moveImageUrl">
-          <Form.Label>URL fotografije</Form.Label>
-          <Form.Control
-            type="url"
-            value={movieImageUrl}
-            onChange={(e) => setMovieImageUrl(e.target.value)}
-          />
-        </Form.Group>
+              <Form.Group className="mb-4" controlId="moveImageUrl">
+                <Form.Label>URL fotografije</Form.Label>
+                <Form.Control
+                  type="url"
+                  value={movieImageUrl}
+                  onChange={(e) => setMovieImageUrl(e.target.value)}
+                />
+              </Form.Group>
 
-        <Button variant="outline-primary" type="submit">
-          Snimi izmjene
-        </Button>
-      </div>
+              <Button
+                variant="outline-primary"
+                type="submit"
+                className="mb-4 w-100"
+              >
+                Snimi izmjene
+              </Button>
+            </div>
+          </Col>
+          <Col md={5}>
+            <div className="image-container mb-4">
+              <img
+                src={movieImageUrl}
+                alt={movieTitle}
+                style={{
+                  objectFit: 'cover',
+                  width: '100%',
+                  height: 'auto',
+                }}
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </Form>
   );
 }
--- src/components/NewMovieForm/NewMovieForm.jsx
+++ src/components/NewMovieForm/NewMovieForm.jsx
@@ -3,6 +3,9 @@ import _ from 'lodash';
 import { useHistory } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function NewMovieForm({ movies, setMovies }) {
   const history = useHistory();
@@ -46,84 +49,96 @@ export default function NewMovieForm({ movies, setMovies }) {
   };
 
   return (
-    <Form className="w-75 d-flex m-auto" onSubmit={onSubmitHandler}>
-      <div className="image-container" style={{ width: '40%' }}>
-        <img
-          src={movieImageUrl}
-          alt={movieTitle}
-          style={{
-            objectFit: 'cover',
-            width: '100%',
-            height: 'auto',
-          }}
-        />
-      </div>
-      <div className="form-fields-containerpe-5 ps-5" style={{ width: '60%' }}>
-        <Form.Group className="mb-4" controlId="movieName">
-          <Form.Label>Naziv filma</Form.Label>
-          <Form.Control
-            type="text"
-            value={movieTitle}
-            placeholder="Unesite ime filma..."
-            onChange={(e) => setMovieTitle(e.target.value)}
-          />
-        </Form.Group>
+    <Form onSubmit={onSubmitHandler}>
+      <Container>
+        <Row>
+          <Col md={7}>
+            <div className="form-fields-container pe-5 ps-5">
+              <Form.Group className="mb-4" controlId="movieName">
+                <Form.Label>Naziv filma</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={movieTitle}
+                  placeholder="Unesite ime filma..."
+                  onChange={(e) => setMovieTitle(e.target.value)}
+                />
+              </Form.Group>
 
-        <Form.Group className="mb-4" controlId="movieYear">
-          <Form.Label>Godina izdavanja</Form.Label>
-          <Form.Control
-            type="number"
-            from={1900}
-            to={2021}
-            value={movieYear}
-            placeholder="Unesite godinu izdavanja..."
-            onChange={(e) => setMovieYear(e.target.value)}
-          />
-        </Form.Group>
+              <Form.Group className="mb-4" controlId="movieYear">
+                <Form.Label>Godina izdavanja</Form.Label>
+                <Form.Control
+                  type="number"
+                  from={1900}
+                  to={2021}
+                  value={movieYear}
+                  placeholder="Unesite godinu izdavanja..."
+                  onChange={(e) => setMovieYear(e.target.value)}
+                />
+              </Form.Group>
 
-        <Form.Group className="mb-4" controlId="moveGenre">
-          <Form.Label>Žanr</Form.Label>
-          <Form.Control
-            type="text"
-            value={movieGenre}
-            placeholder="Unesite žanr..."
-            onChange={(e) => setMovieGenre(e.target.value)}
-          />
-        </Form.Group>
+              <Form.Group className="mb-4" controlId="moveGenre">
+                <Form.Label>Žanr</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={movieGenre}
+                  placeholder="Unesite žanr..."
+                  onChange={(e) => setMovieGenre(e.target.value)}
+                />
+              </Form.Group>
 
-        <Form.Group className="mb-4" controlId="moveDirector">
-          <Form.Label>Režija</Form.Label>
-          <Form.Control
-            type="text"
-            value={movieDirector}
-            placeholder="Unesite ime režisera/režiserke..."
-            onChange={(e) => setMovieDirector(e.target.value)}
-          />
-        </Form.Group>
+              <Form.Group className="mb-4" controlId="moveDirector">
+                <Form.Label>Režija</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={movieDirector}
+                  placeholder="Unesite ime režisera/režiserke..."
+                  onChange={(e) => setMovieDirector(e.target.value)}
+                />
+              </Form.Group>
 
-        <Form.Group className="mb-4" controlId="moveActors">
-          <Form.Label>Glumci</Form.Label>
-          <Form.Control
-            type="text"
-            value={movieActors}
-            placeholder="Unesite listu glumaca..."
-            onChange={(e) => setMovieActors(e.target.value)}
-          />
-        </Form.Group>
+              <Form.Group className="mb-4" controlId="moveActors">
+                <Form.Label>Glumci</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={movieActors}
+                  placeholder="Unesite listu glumaca..."
+                  onChange={(e) => setMovieActors(e.target.value)}
+                />
+              </Form.Group>
 
-        <Form.Group className="mb-4" controlId="moveImageUrl">
-          <Form.Label>URL fotografije</Form.Label>
-          <Form.Control
-            type="url"
-            value={movieImageUrl}
-            onChange={(e) => setMovieImageUrl(e.target.value)}
-          />
-        </Form.Group>
+              <Form.Group className="mb-4" controlId="moveImageUrl">
+                <Form.Label>URL fotografije</Form.Label>
+                <Form.Control
+                  type="url"
+                  value={movieImageUrl}
+                  onChange={(e) => setMovieImageUrl(e.target.value)}
+                />
+              </Form.Group>
 
-        <Button variant="outline-primary" type="submit">
-          Dodaj film
-        </Button>
-      </div>
+              <Button
+                variant="outline-primary"
+                type="submit"
+                className="mb-4 w-100"
+              >
+                Dodaj film
+              </Button>
+            </div>
+          </Col>
+          <Col md={5}>
+            <div className="image-container mb-4">
+              <img
+                src={movieImageUrl}
+                alt={movieTitle}
+                style={{
+                  objectFit: 'cover',
+                  width: '100%',
+                  height: 'auto',
+                }}
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </Form>
   );
 }
